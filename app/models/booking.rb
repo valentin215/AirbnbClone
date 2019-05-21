d993b28b4900d0a964f14a1e2ba70d4cc95956d6
Carlos Rico Gomez <c.rico1@hotmail.co.uk>
2019-05-21 15:22:59 +0200

    Added data in seed file

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :flat
   belongs_to :user
-  validates_uniqueness_of :flat_id, :scope [:user_id]
+  # validates_uniqueness_of :flat_id, :scope [:user_id]
 end
